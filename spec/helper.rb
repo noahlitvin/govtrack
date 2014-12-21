c8bbc144d1bb504624c075952d2a59e919d1d47c
Tim Booher <ads@theboohers.org>
2014-12-21 23:25:22 +0000

    vcr at least runs

--- spec/helper.rb
+++ spec/helper.rb
@@ -3,3 +3,12 @@ require 'rspec'
 require 'fakeweb'
 #require 'typhoeus'
 require 'vcr'
+
+VCR.configure do |c|
+  c.cassette_library_dir = 'spec/vcr'
+  # HTTP request service
+  c.hook_into :fakeweb
+  c.configure_rspec_metadata!
+  c.allow_http_connections_when_no_cassette = false
+  c.default_cassette_options = {:record => :new_episodes}
+end
--- spec/support/vcr_setup.rb
+++
@@ -1,6 +0,0 @@
-VCR.configure do |c|
-  c.cassette_library_dir = 'spec/vcr'
-  # HTTP request service
-  c.hook_into :fakeweb
-#   c.default_cassette_options = {:record => :new_episodes}
-end
