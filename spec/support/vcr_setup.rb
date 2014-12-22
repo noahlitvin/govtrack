a923993fd911a0b2f22e0ba04d23455a1344f0a6
Tim Booher <tim@theboohers.org>
2014-12-21 20:42:26 -0500

    update ready

--- spec/support/vcr_setup.rb
+++ spec/support/vcr_setup.rb
@@ -1,6 +1,11 @@
 VCR.configure do |c|
   c.cassette_library_dir = 'spec/vcr'
   # HTTP request service
-  c.hook_into :fakeweb
+  c.register_request_matcher :uri do |request_1, request_2|
+    # debugger # so you can inspect the requests here
+    request_1.uri == request_2.uri
+  end
+  # c.hook_into :fakeweb
+  c.stub_with :fakeweb
 #   c.default_cassette_options = {:record => :new_episodes}
 end
