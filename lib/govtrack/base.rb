22e0a6e702aeca4650395b70d96b6f9501af3808
Tim Booher <tim@theboohers.org>
2015-02-01 14:58:04 -0500

    updated

--- lib/govtrack/base.rb
+++ lib/govtrack/base.rb
@@ -24,7 +24,7 @@ module GovTrack
       args[:limit] ||= 500 if block_given? #default to queries of 500 when a block is given
       
       response = get("/#{self.demodulized_name}/?#{URI.escape(URI.encode_www_form(args))}")
-      puts "requesting: #{base_uri}/#{self.demodulized_name}/?#{URI.escape(URI.encode_www_form(args))}"
+      puts "requesting: #{base_uri}/#{self.demodulized_name}/#{URI.escape(URI.encode_www_form(args))}"
       paginated_list = GovTrack::PaginatedList.new(self,response["meta"],response["objects"])
       
       if block_given?
